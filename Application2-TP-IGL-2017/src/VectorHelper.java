08e8813ce73461188a7e4cfd5ea7c98cf2263846
kaciyounes <fm_kaci@esi.dz>
2017-10-18 22:33:35 +0200

    Add files via upload
    Ajout de la méthode qui tri le vecteur

--- Application2-TP-IGL-2017/src/VectorHelper.java
+++ Application2-TP-IGL-2017/src/VectorHelper.java
@@ -1,69 +1,36 @@
 import java.util.ArrayList;
-import java.util.Iterator;
 
 /**
  * Created by acer on 18/10/2017.
  */
-public class VectorHelper
-{
-    import java.util.ArrayList;
-
-    public class VectorHelper
+public class VectorHelper {
+    /**
+     * trier un vecteur donné en parametre selon la méthode des bulles
+     * @param tab vecteur
+     */
+    public static void triVect(ArrayList<Integer> tab)
     {
-        public static void triVect(ArrayList<Integer> tab)
-        {}
-
-        public static ArrayList<Integer> somVect(ArrayList<Integer> tab1,ArrayList<Integer> tab2)throws TailleException
-        {}
-
-        public static void invVect(ArrayList<Integer> tab)
-        {
-            int i,v,j=tab.size()-1;
-            for(i=0;i<tab.size()/2;i++){
-                v=tab.get(i);
-                tab.set(i,tab.get(j));
-                tab.set(j,v);
-            }
-        }
-
-        public static ArrayList<Integer> extVect(ArrayList<Integer> tab)
-        {
-            int max=0,min=0;
-            ArrayList<Integer> extremums=new ArrayList<Integer>();
-            int entier;
-            if(tab.size()>0)
-            {
-                max=tab.get(0);
-                min=tab.get(0);
-                Iterator<Integer> it=tab.iterator();
-                while (it.hasNext())
-                {
-                    entier=it.next();
-                    if (entier>max) max=entier;
-                    if (entier<min) min=entier;
+        int i,j,v;
+        for(i=0;i<tab.size()-1;i++){
+            for(j=0;j<tab.size()-1-i;j++){
+                if(tab.get(j)>tab.get(j+1)){
+                    v=tab.get(j);
+                    tab.set(j,tab.get(j));
+                    tab.set(j+1,v);
                 }
-                extremums.add(0,min);
-                extremums.add(1,max);
-                return extremums;
             }
-            else
-            {
-                return null;
-            }
-        }
-
-        public static void formulVect(ArrayList<Integer> tab)
-        {
-            int size=tab.size();
-            for(int i=0;i<size;i++)
-            {
-                tab.add(i,(tab.get(i)^2)-1);
-            }
-        }
-
-        public static int formule(int n)
-        {
-            return n*n-1;
         }
     }
-}
+    public static ArrayList<Integer> somVect(ArrayList<Integer> tab1,ArrayList<Integer> tab2)throws TailleException
+    {}
+    public static void invVect(ArrayList<Integer> tab)
+    {}
+    public static ArrayList<Integer> extVect(ArrayList<Integer> tab)
+    {}
+    public static void formulVect(ArrayList<Integer> tab)
+    {}
+    public static int formule(int n)
+    {}
+
+
+    }
