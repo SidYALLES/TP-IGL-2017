2a1b6fc12950a467e25de15ee87b5d9ab8882fcb
SidahmedYS <32796746+SidahmedYS@users.noreply.github.com>
2017-10-18 17:20:04 +0100

    Le projet initial
    Contient le projet correspondant en java sur intellij

---
+++ Application2-TP-IGL-2017/src/VectorHelper.java
@@ -0,0 +1,28 @@
+/**
+ * Created by acer on 18/10/2017.
+ */
+public class VectorHelper
+{
+    import java.util.ArrayList;
+
+    public class VectorHelper
+    {
+        public static void triVect(ArrayList<Integer> tab)
+        {}
+
+        public static ArrayList<Integer> somVect(ArrayList<Integer> tab1,ArrayList<Integer> tab2)throws TailleException
+        {}
+
+        public static void invVect(ArrayList<Integer> tab)
+        {}
+
+        public static ArrayList<Integer> extVect(ArrayList<Integer> tab)
+        {}
+
+        public static void formulVect(ArrayList<Integer> tab)
+        {}
+
+        public static int formule(int n)
+        {}
+    }
+}
