cbc2ad97e2bc0a4cb40c84a9a72124bffc226af1
kaciyounes <fm_kaci@esi.dz>
2017-10-24 22:21:57 +0200

    Update TailleException.java

--- Application2-TP-IGL-2017/src/TailleException.java
+++ Application2-TP-IGL-2017/src/TailleException.java
@@ -1,5 +1,5 @@
 public class TailleException extends Exception {
     public TailleException(){
-        super("La taille des deux vecteurs est differentes,impossible de faire la somme ");
+        super("La taille des deux vecteurs est differentes,impossible de faire la somme");
     }
 }
